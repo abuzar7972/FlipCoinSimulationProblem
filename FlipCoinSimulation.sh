3c06bd5357a9a9f0658a356ec71a49cc61bffa58
abuzar7972 <abushaikh7972@gmail.com>
2021-06-12 16:10:18 +0530

    Displayed number of heads and tails multiple times

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,8 +1,17 @@
 #!/bin/bash -x
+heads=0
+tails=0
+read -p "Enter number of loops:: " n
+for ((counter=1; counter<=$n; counter++))
+do
 randomCheck=$((RANDOM%2))
 if [ $randomCheck -eq 0 ]
 	then
-	echo "Heads"
+	heads=$(($heads+1))
 	else
-	echo "Tails"
+	tails=$(($tails+1))
 fi
+done
+
+echo "Number of heads:: " $heads
+echo "Number of tails:: " $tails
