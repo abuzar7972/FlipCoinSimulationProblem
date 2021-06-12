58d789bc5cc866d1afbb55f3f6ac416b8d425aaf
abuzar7972 <abushaikh7972@gmail.com>
2021-06-12 16:36:39 +0530

    Simulated for result must be grater than 2

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -15,8 +15,6 @@ then
 	break
 fi 
 done
-echo "Number of heads:: " $heads
-echo "Number of tails:: " $tails
 if [ $heads -ge $tails ]
 then
 	result=$(($heads-$tails))
@@ -29,3 +27,36 @@ if [ $result -eq 0 ]
 then
 	echo "Tie"
 fi
+while [ $result ]
+do
+	if [ $result -ge 2 ]
+	then
+	break
+else
+while [ $heads ]
+do
+randomCheck=$((RANDOM%2))
+if [ $randomCheck -eq 0 ]
+        then
+        heads=$(($heads+1))
+        else
+        tails=$(($tails+1))
+fi
+if [ $heads -eq 21 ]&[ $tails -eq 21 ]
+then
+        break
+fi
+done
+if [ $heads -ge $tails ]
+then
+        result=$(($heads-$tails))
+        echo "Heads Won by:: " $result
+        else
+        result=$(($tails-$heads))
+        echo "Tails Won by:: " $result
+fi
+if [ $result -eq 0 ]
+then
+        echo "Tie"
+fi
+done
